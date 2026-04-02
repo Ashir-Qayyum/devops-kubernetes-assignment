7ad64f985a397181cb1bd1f35201a3d59a89944c
ashir-qayyum <muhammadashir2004@gmail.com>
2026-04-02 12:16:10 +0000

    Part 07 - Testing CI/CD with Webhook - att 08 build 07

--- python-basic-app.py
+++ python-basic-app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Ashir app for assignment 07. Attempt 07 - Build 06. Deploying in Minikube at MacOS with Webhook."
+    return "Welcome to Ashir app for assignment 07. Attempt 08 - Build 07. Deploying in Minikube at MacOS with Webhook."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
